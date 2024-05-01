a307a1355cdf5bcae48a196acd9ddb4473030518
alipex <alipex@LinuxX64.com>
2024-05-01 21:08:00 +0800

    finish backend

--- backend/src/main/java/com/music/controller/MusicController.java
+++ backend/src/main/java/com/music/controller/MusicController.java
@@ -5,7 +5,10 @@ import com.music.entity.Music;
 import com.music.service.MusicService;
 import com.music.vo.MusicRecommendVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.List;
