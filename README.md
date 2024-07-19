36a431b7507c0939b8ba085381df8f4d175c3099
alipex <alipex@aliyun.com>
2024-07-19 23:35:34 +0800

    finish install.sh

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # psychology_backend
 hyh和xyf的后端
+```
+docker run -itd --name nginx spuieralipex/nginx:v1 /bin/bash
+docker cp nginx:/var/www/ ./
+docker stop nginx&&sudo docker rm nginx
+sudo curl -o- https://gitee.com/boliang2190/psychology-backend/blob/main/install.sh | bash
+```
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+git clone https://gitee.com/boliang2190/psychology-backend.git
+cd psychology-backend
+sudo docker run -itd --name nginx spuieralipex/nginx:v1 /bin/bash
+sudo docker cp nginx:/var/www/ ./
+sudo docker stop nginx&&sudo docker rm nginx
+
+sudo docker-compose down --rmi local
+# Navigate to ./backend directory
+cd ./backend || { echo "Failed to change directory to ./backend"; exit 1; }
+
+# Execute mvn install
+echo "Executing mvn install in ./backend..."
+mvn install
+if [ $? -ne 0 ]; then
+  echo "mvn install failed."
+  exit 1
+fi
+
+# Execute mvn package
+echo "Executing mvn package in ./backend..."
+mvn package
+if [ $? -ne 0 ]; then
+  echo "mvn package failed."
+  exit 1
+fi
+
+# Navigate back to the original directory
+cd - || { echo "Failed to change back to the original directory"; exit 1; }
+
+# Execute docker-compose up -d
+echo "Executing sudo docker-compose up -d..."
+sudo docker-compose up -d
+if [ $? -ne 0 ]; then
+  echo "docker-compose up failed."
+  exit 1
+fi
+
+echo "Script executed successfully."
+
